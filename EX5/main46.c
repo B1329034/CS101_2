2901c865bdf67b1d94fa03684acc5240e3140a6e
B1329034 <jtkao.tw@gmail.com>
2025-10-16 16:30:39 +0800

    Update main46.c

--- EX5/main46.c
+++ EX5/main46.c
@@ -8,12 +8,10 @@ void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod){
     towerOfHanoi(n-1,from_rod,to_rod, aux_rod);
     printf("\n Move disk %d from rod %c to %c", n,from_rod, to_rod);
     towerOfHanoi(n-1,aux_rod, to_rod,from_rod);
-    
+}   
 int main(){
     int n = 3;
-    towerOfHanoi(n, 'A'.'B','C');
-    return 0
+    towerOfHanoi(n, 'A','B','C');
+    return 0;
 }    
-    
-    
-}
+  
